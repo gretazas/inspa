3e0d9835204ae5a5c1122080db30253327902e4e
gretazas <esminis@yahoo.com>
2022-10-23 07:30:04 +0000

    setting debug false

--- Inspa/settings.py
+++ Inspa/settings.py
@@ -28,16 +28,16 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['inspasite.herokuapp.com', 'localhost']
 CSRF_TRUSTED_ORIGINS = [
-                'https://800o-gretazas-inspa-stze9rw27qr.ws-eu72.gitpod.io',
+                'https://8000-gretazas-inspa-stze9rw27qr.ws-eu72.gitpod.io',
                 'https://*.*.gitpod.io', 'http://127.0.0.1:8000/',
                 'http://127.0.0.1:800o/'
                         ]
 PAPERLESS_CSRF_TRUSTED_ORIGINS = [
-                'http://127.0.0.1:8001/',
+                'http://127.0.0.1:8000/',
                         ]
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 ACCOUNT_EMAIL_VARIFICATION = 'none'
